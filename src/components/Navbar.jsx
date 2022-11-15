a03a76ad6ceda328e9f29d43e6ff61649ee37835
Rafael Longeville <rafael.longeville.pro@gmail.com>
2022-11-15 07:25:21 +0100

    image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,87 @@
-import React from "react";
 import {
-  Flex,
-  Text,
   Box,
-  Grid,
-  GridItem,
-  Container,
-  Divider,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Image,
 } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon, AddIcon } from "@chakra-ui/icons";
 
-const Navbar = () => {
+import Logo from "../assets/DevTekLogo.webp";
+
+const Links = ["Services", "Book a call", "Testimonials"];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <Box pb="10vh">
-        <Container maxW={"7xl"} pt="1vh">
-          <Grid
-            templateColumns={{ base: "1fr 1fr", md: "1fr 1fr", lg: "3fr 1fr" }}
-          >
-            <GridItem>
-              <Text
-                color="#576CC0"
-                fontSize={{ base: "xl", md: "2xl", lg: "4xl" }}
-                fontWeight="bold"
-              >
-                Devtek Solutions
-              </Text>
-            </GridItem>
-            <GridItem align="center">
-              <Flex justify="space-evenly" align="center" h="100%">
-                <Box>
-                  <a href="#services">
-                    <Text
-                      _hover={{
-                        textColor: "white",
-                      }}
-                      color="whiteAlpha.800"
-                      fontSize={{ base: "xs", md: "md", lg: "xl" }}
-                    >
-                      Services
-                    </Text>
-                  </a>
-                </Box>
-                <Box>
-                  <a href="#testimonials">
-                    <Text
-                      _hover={{
-                        textColor: "white",
-                      }}
-                      color="whiteAlpha.800"
-                      fontSize={{ base: "xs", md: "md", lg: "xl" }}
-                    >
-                      Testimonials
-                    </Text>
-                  </a>
-                </Box>
-              </Flex>
-            </GridItem>
-          </Grid>
-        </Container>
-        <Divider />
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justify="center">
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack alignItems={"center"} w="90%" justifyContent={"space-between"}>
+            <Box pl={{base: "5vw", md: null, lg: null}}>
+              <Image
+                src={Logo}
+                h={{ base: "5vh" }}
+                w={{ base: "15vh" }}
+              ></Image>
+            </Box>
+            <HStack
+            
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
     </>
   );
-};
-
-export default Navbar;
+}
